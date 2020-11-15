5fea2eba27798e1c21181786d0de656e21bd190b
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-11-15 13:41:27 +0100

    conditionals

--- conditionals.sh
+++ conditionals.sh
@@ -10,3 +10,15 @@ if [[ 10 -eq 10 ]]; then
 else
 	echo "false"
 fi
+
+check_status() {
+    local status = $(curl -ILs https://example.org | head -n 1 | cut -d ' ' -f 2)
+    if [[ $status -lt 200 ]] || [[ $status -gt 299 ]]; then
+        echo "$1 failed with a $status"
+        return 1
+    else
+        echo "$1 succeeded with a $status"
+    fi
+}
+
+check_status
